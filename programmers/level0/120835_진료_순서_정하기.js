2da1cce84260fb0de472fe8e2c3c3c0c851b7fba
Lee Jooam <jwam.lee@woowahan.com>
2023-10-01 00:52:12 +0900

    프로그래머스 level0 120835 문제 오류 수정

--- programmers/level0/120835_진료_순서_정하기.js
+++ programmers/level0/120835_진료_순서_정하기.js
@@ -3,7 +3,7 @@
 function solution(emergency) {
   const sortedEmergency = [...emergency].sort((a, b) => b - a);
 
-  return sortedEmergency.map(
-    (item) => emergency.findIndex((data) => data === item) + 1
+  return emergency.map(
+    (item) => sortedEmergency.findIndex((data) => data === item) + 1
   );
 }
